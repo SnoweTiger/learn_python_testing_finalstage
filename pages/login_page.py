09e0428df0303ec719c8eb01d07f2036878925b8
SnoweTiger <snowetiger@gmail.com>
2020-07-15 17:48:03 +0300

    added redirect & .should_be_register_form

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,6 +3,11 @@ from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
 
+    def should_be_login_page1(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
     def should_be_login_url(self):
         assert ("login" in self.browser.current_url),"URL does not contain login"
 
