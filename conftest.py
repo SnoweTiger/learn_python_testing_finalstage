eaf48ddbd8763a8943c3ea516cad83c3700105e2
SnoweTiger <snowetiger@gmail.com>
2020-07-15 15:16:33 +0300

    locator added

--- conftest.py
+++ conftest.py
@@ -19,7 +19,7 @@ def browser(request):
     browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        # browser = webdriver.Chrome()
 
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
@@ -27,13 +27,15 @@ def browser(request):
 
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+        # browser = webdriver.Firefox()
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", user_language)
         browser = webdriver.Firefox(firefox_profile=fp)
+
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
 
     yield browser
+
     print("\nquit browser..")
     browser.quit()
