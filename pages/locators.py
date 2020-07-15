bba3ebb3aad84146d2f7020a4e7362d26f7dd0fe
SnoweTiger <snowetiger@gmail.com>
2020-07-15 17:20:14 +0300

    added login_page

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,12 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    #for login form
+    LOGIN_FORM_MAIL = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_FORM_PASS = (By.CSS_SELECTOR, "#id_login-password")
+    #for registration form
+    REG_FORM_MAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REG_FORM_PASS1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    REG_FORM_PASS2 = (By.CSS_SELECTOR, "#id_registration-password2")
---
+++ pages/login_page.py
@@ -0,0 +1,16 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+class LoginPage(BasePage):
+
+    def should_be_login_url(self):
+        assert ("login" in self.browser.current_url),"URL does not contain login"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM_MAIL),"Login form: mail input is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM_PASS),"Login form: password input is not presented"
+
+    def should_be_register_form(self):
+         assert self.is_element_present(*LoginPageLocators.REG_FORM_MAIL),"Registration form: mail input is not presented"
+         assert self.is_element_present(*LoginPageLocators.REG_FORM_PASS1),"Registration form: password input is not presented"
+         assert self.is_element_present(*LoginPageLocators.REG_FORM_PASS2),"Registration form: confirm password input is not presented"
